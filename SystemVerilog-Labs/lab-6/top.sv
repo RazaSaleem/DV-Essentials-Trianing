4aa65087c0fe893687595e7b35d80ce815bc5a33
Saleem Raza <95694625+RazaSaleem@users.noreply.github.com>
2022-08-07 18:34:00 +0500

    Create top.sv

---
+++ SystemVerilog-Labs/lab-6/top.sv
@@ -0,0 +1,36 @@
+///////////////////////////////////////////////////////////////////////////
+//
+// File name   : top.sv
+// Title       : top module for Memory labs 
+// Project     : SystemVerilog Training
+// Description : Defines the top module for memory labs
+// Notes       :
+// Memory Lab - top-level 
+// A top-level module which instantiates the memory and mem_test modules
+// 
+///////////////////////////////////////////////////////////////////////////
+
+module top;
+// SYSTEMVERILOG: timeunit and timeprecision specification
+timeunit 1ns;
+timeprecision 1ns;
+
+// SYSTEMVERILOG: logic and bit data types
+bit         clk;
+wire       read;
+wire       write;
+wire [4:0] addr;
+
+wire [7:0] data_out;      // data_from_mem
+wire [7:0] data_in;       // data_to_mem
+
+// SYSTEMVERILOG:: implicit .* port connections
+mem_test test (.*);
+
+// SYSTEMVERILOG:: implicit .name port connections
+mem memory ( .clk, .read, .write, .addr,
+              .data_in, .data_out
+            );
+
+always #5 clk = ~clk;
+endmodule
