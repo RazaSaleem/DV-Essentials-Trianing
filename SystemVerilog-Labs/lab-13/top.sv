18d76c87c30b48529b7aae515442b7ddf2d43b80
Saleem Raza <95694625+RazaSaleem@users.noreply.github.com>
2022-08-07 18:47:18 +0500

    Create top.sv

---
+++ SystemVerilog-Labs/lab-13/top.sv
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////////////////////
+
+// File name   : top.sv
+// Title       : top Module for memory
+// Project     : SystemVerilog Training
+
+// Description : Defines the top module for memory with interface, clk port,
+// modport and methods
+// Notes       :
+// Memory Lab - top-level
+// A top-level module which instantiates the memory and mem_test modules
+//
+///////////////////////////////////////////////////////////////////////////
+`include "interface.sv"
+
+module top;
+// SYSTEMVERILOG: timeunit and timeprecision specification
+timeunit 1ns;
+timeprecision 1ns;
+
+// SYSTEMVERILOG: logic and bit data types
+bit clk;
+
+always #5 clk = ~clk;
+
+// SYSTEMVERILOG: interface instance
+mem_intf mbus (clk);
+
+mem_test mtest (.mbus(mbus.tb));
+
+mem m1  (.mbus(mbus.mem));
+
+endmodule
